4e9d149f5691e7bbc1dea0ed67c86ac237f551bd
retr0gitpack <aaryaman.nigam@firminiq.com>
2021-06-03 14:22:15 +0530

    user create script added.

---
+++ iam_user_create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+user=test2
+
+pass=Omrontest!@
+
+policy=arn:aws:iam::aws:policy/AmazonS3FullAccess
+
+group=arn:aws:iam::728483987316:group/Restricted_Group
+
+echo "User: $user"
+
+aws iam create-user --user-name $user
+
+aws iam attach-user-policy --user-name $user --policy-arn $policy
+
+aws iam add-user-to-group --user-name $user --group-name $group
+
+aws iam update-login-profile --user-name $user --password $pass --password-reset-required yes
